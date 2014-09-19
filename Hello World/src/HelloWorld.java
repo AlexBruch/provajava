42673456d31db157d4ba88f83df0c48c2ef98ea6
46143994Z <46143994Z@a27o14.novaulas.stucom.com>
2014-09-19 18:40:18 +0200

    Wasaaaa

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("This is the first change, to demonstrate git");
-		System.out.println("This is Sparta!!!");
+		int a=0;
+		if(a==1)
+			System.out.println("This is Sparta!!!");
 		System.out.println("Are you sure?");
 	}
 
