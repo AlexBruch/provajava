57b7f2a94c3c40d2b99d6fa4b3f4b642798a8f1b
AlexBruch <alexbruchforrellad@gmail.com>
2014-09-19 17:38:49 +0200

    Second change

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println("This is the first change, to demonstrate git");
 	}
 
 }
